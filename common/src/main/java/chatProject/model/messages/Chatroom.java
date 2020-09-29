2199363f89c4f746df89ad2f5512d0ab5ae3febb
Edouard <edouardclisson@gmail.com>
2020-09-29 15:03:31 +0200

    Fix: debug common test

--- common/src/main/java/chatProject/model/messages/Chatroom.java
+++ common/src/main/java/chatProject/model/messages/Chatroom.java
@@ -37,6 +37,7 @@ public class Chatroom<T> {
      * @return the name of the chatroom
      */
     public String getName() {
+        return name;
     }
 
     /**
@@ -63,7 +64,7 @@ public class Chatroom<T> {
      * @return the new message created
      */
     public Message<T> addMessage(UserInfo userInfo, T content) {
-        final Message<T> message = new Message<>(0, null, content);
+        final Message<T> message = new Message<>(0, userInfo, content);
         this.messages.add(message);
         return message;
     }
