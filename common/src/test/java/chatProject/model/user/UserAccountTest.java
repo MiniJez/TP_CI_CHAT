faf44b509927e71c12625e25ec1922c52f60be7f
Marc-Antoine <marco38000@gmail.com>
2020-10-16 10:14:51 +0200

    Feat : Add test user account

--- common/src/test/java/chatProject/model/user/UserAccountTest.java
+++ common/src/test/java/chatProject/model/user/UserAccountTest.java
@@ -2,6 +2,8 @@ package chatProject.model.user;
 
 import org.junit.Test;
 
+import java.util.Objects;
+
 import static org.junit.Assert.*;
 
 public class UserAccountTest {
@@ -19,21 +21,22 @@ public class UserAccountTest {
     @Test
     public void testEquals() {
 
-        final UserAccount user1 = new UserAccount(1, "MyUser");
-        final UserAccount user2 = new UserAccount(2, "MyUser");
+        final UserAccount user1 = new UserAccount(1, "MyUser1");
+        final UserAccount user2 = new UserAccount(1, "MyUser2");
 
-        assertEquals("2 accounts are equal if they have the same username",
-                user1.getUsername(), user2.getUsername());
+        assertTrue("2 accounts are equal if they have the same id",
+                user1.equals(user2));
     }
 
     @Test
     public void testHashCode() {
 
-        final UserAccount user1 = new UserAccount(1, "MyUser");
-        final UserAccount user2 = new UserAccount(2, "MyUser");
+        final UserAccount user1 = new UserAccount(1, "MyUser1");
+        final UserAccount user2 = new UserAccount(2, "MyUser1");
+        int hashUser = Objects.hash(user2.getUsername());
 
         assertEquals("2 accounts with the same username should have the same hashcode",
-                user1.hashCode(), user2.hashCode());
+                user1.hashCode(), hashUser);
     }
 
     @Test
@@ -42,5 +45,13 @@ public class UserAccountTest {
         final UserAccount user = new UserAccount(0, "MyUser");
         assertEquals("The toString() method of a UserAccount should print the username",
                 "MyUser", user.toString());
+    }
+
+    @Test
+    public void testGetId() {
+
+        final UserAccount user = new UserAccount(0, "MyUser");
+        assertEquals("The id should be the one set in the constructor",
+                0, user.getId());
     }
 }
